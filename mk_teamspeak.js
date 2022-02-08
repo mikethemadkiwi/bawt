1b28109a88ee2bab87f94f0be364b3eb7cdc4a86
Mike The Madkiwi <mike@hildyard.ca>
2022-02-08 13:39:20 -0500

    ts

--- mk_teamspeak.js
+++ mk_teamspeak.js
@@ -1,22 +1,14 @@
-const fs = require('fs');
-const path = require('path');
-const { TeamSpeak, QueryProtocol } = require("ts3-nodejs-library")
-const { EventEmitter } = require("events");
 //
-class mkTeamspeak extends EventEmitter {
-    constructor(authfile){
-        super();
-        
-////
 const { TeamSpeak, QueryProtocol } = require("ts3-nodejs-library")
+const authfile = require('../auths/teamspeak/yggylogin.json')
 TeamSpeak.connect({
-        host: "localhost",
+        host: authfile.host,
         protocol: QueryProtocol.RAW, //optional
-        queryport: 10011, //optional
-        serverport: 9987,
-        username: "YggY",
-        password: "o6o20Idt",
-        nickname: "YggY"
+        queryport: authfile.queryport, //optional
+        serverport: authfile.serverport,
+        username: authfile.username,
+        password: authfile.password,
+        nickname: authfile.nickname
 }).then(async teamspeak => {
         teamspeak.whoami().then(whoami => {
                 teamspeak.clientMove(whoami.clientId, 2, '', false)
@@ -37,13 +29,3 @@ TeamSpeak.connect({
 }).catch(e => {
         console.log(e)       
 })
-
-// const clients = await teamspeak.clientList({ clientType: 0 })
-// clients.forEach(client => {
-//         console.log(client.nickname)
-//         // client.message(`hello there ${client.nickname}... i SEEEE YOUUUUU`)
-// })
-    }
-};
-//
-module.exports = mkTeamspeak;
