d05aac6d392157b43ab6630e87a070349e2643cc
Mike The Madkiwi <mike@hildyard.ca>
2022-03-02 08:24:48 -0500

    pubsub upgrade?

--- tmiexample.js
+++ tmiexample.js
@@ -318,7 +318,9 @@ class PubLib {
                     // console.log(data);
                     console.log('PUBSUB','RESPONSE: ' + (data.error ? data.error : 'OK'));
                 } else if (data.type == 'MESSAGE') {
+                    let topic = JSON.parse(data.data.topic);
                     var msg = JSON.parse(data.data.message);
+                    console.log('pubsub', topic)
                     ///
                     if(msg.hasOwnProperty('message_type')){
                        switch(msg.message_type){
