04e300e487fb46190b795a83e5ab477e40c0f8ea
Mike The Madkiwi <mike@hildyard.ca>
2025-03-07 12:49:27 -0500

    fixed shoutouts

--- twitch.js
+++ twitch.js
@@ -157,7 +157,6 @@ class initSocket {
         4006: 'Network error',
         4007: 'Invalid Reconnect'
     }
-
     constructor(connect) {
         this._events = {};
 
@@ -165,128 +164,80 @@ class initSocket {
             this.connect();
         }
     }
-
     connect(url, is_reconnect) {
         this.eventsub = {};
         this.counter++;
-
         url = url ? url : 'wss://eventsub.wss.twitch.tv/ws';
         is_reconnect = is_reconnect ? is_reconnect : false;
-
         console.log(`Connecting to ${url}|${is_reconnect}`);
         this.eventsub = new ws(url);
         this.eventsub.is_reconnecting = is_reconnect;
         this.eventsub.counter = this.counter;
-
         this.eventsub.addEventListener('open', () => {
             console.log(`Opened Connection to Twitch`);
         });
-        // https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/close_event
-        // https://github.com/Luka967/websocket-close-codes
         this.eventsub.addEventListener('close', (close) => {
-            console.log('EventSub close', close, this.eventsub);
-            console.log(`${this.eventsub.twitch_websocket_id}/${this.eventsub.counter} Connection Closed: ${close.code} Reason - ${this.closeCodes[close.code]}`);
-
             if (!this.eventsub.is_reconnecting) {
-                console.log(`${this.eventsub.twitch_websocket_id}/${this.eventsub.counter} Is not reconnecting, auto reconnect`);
-                //new initSocket();
                 this.connect();
             }
-
             if (close.code == 1006) {
-                // do a single retry
                 this.eventsub.is_reconnecting = true;
             }
         });
-        // https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/error_event
         this.eventsub.addEventListener('error', (err) => {
             console.log(err);
             console.log(`${this.eventsub.twitch_websocket_id}/${this.eventsub.counter} Connection Error`);
         });
-        // https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/message_event
         this.eventsub.addEventListener('message', (message) => {
-            //log('Message');
-            //console.log(this.eventsub.counter, message);
             let { data } = message;
             data = JSON.parse(data);
-
             let { metadata, payload } = data;
             let { message_id, message_type, message_timestamp } = metadata;
-            //log(`Recv ${message_id} - ${message_type}`);
-
             switch (message_type) {
                 case 'session_welcome':
                     let { session } = payload;
                     let { id, keepalive_timeout_seconds } = session;
-
-                    console.log(`${this.eventsub.counter} This is Socket ID ${id}`);
                     this.eventsub.twitch_websocket_id = id;
-
-                    console.log(`${this.eventsub.counter} This socket declared silence as ${keepalive_timeout_seconds} seconds`);
-
                     if (!this.eventsub.is_reconnecting) {
-                        console.log('Dirty disconnect or first spawn');
                         this.emit('connected', id);
-                        // now you would spawn your topics
                     } else {
                         this.emit('reconnected', id);
-                        // no need to spawn topics as carried over
                     }
-
                     this.silence(keepalive_timeout_seconds);
-
                     break;
                 case 'session_keepalive':
-                    //log(`Recv KeepAlive - ${message_type}`);
                     this.emit('session_keepalive');
                     this.silence();
                     break;
-
                 case 'notification':
-                    //console.log('notification', metadata, payload);
-                    //log(`${this.eventsub.twitch_websocket_id}/${this.eventsub.counter} Recv notification`);// ${JSON.stringify(payload)}`);
-
                     let { subscription, event } = payload;
                     let { type } = subscription;
-
                     this.emit('notification', { metadata, payload });
                     this.emit(type, { metadata, payload });
                     this.silence();
-
                     break;
-
                 case 'session_reconnect':
                     this.eventsub.is_reconnecting = true;
-
                     let reconnect_url = payload.session.reconnect_url;
-
                     console.log('Connect to new url', reconnect_url);
                     console.log(`${this.eventsub.twitch_websocket_id}/${this.eventsub.counter} Reconnect request ${reconnect_url}`)
-
-                    //this.eventsub.close();
-                    //new initSocket(reconnect_url, true);
                     this.connect(reconnect_url, true);
-
                     break;
                 case 'websocket_disconnect':
                     console.log(`${this.eventsub.counter} Recv Disconnect`);
                     console.log('websocket_disconnect', payload);
-
                     break;
-
                 case 'revocation':
                     console.log(`${this.eventsub.counter} Recv Topic Revocation`);
                     console.log('revocation', payload);
                     this.emit('revocation', { metadata, payload });
                     break;
-
                 default:
                     console.log(`${this.eventsub.counter} unexpected`, metadata, payload);
                     break;
             }
         });
     }
-
     trigger() {
         // this function lets you test the disconnect on send method
         this.eventsub.send('cat');
@@ -294,7 +245,6 @@ class initSocket {
     close() {
         this.eventsub.close();
     }
-
     silenceHandler = false;
     silenceTime = 10;// default per docs is 10 so set that as a good default
     silence(keepalive_timeout_seconds) {
@@ -308,23 +258,19 @@ class initSocket {
             this.close();// close it and let it self loop
         }, (this.silenceTime * 1000));
     }
-
     on(name, listener) {
         if (!this._events[name]) {
             this._events[name] = [];
         }
-
         this._events[name].push(listener);
     }
     emit(name, data) {
         if (!this._events[name]) {
             return;
         }
-
         const fireCallbacks = (callback) => {
             callback(data);
         };
-
         this._events[name].forEach(fireCallbacks);
     }
 }
@@ -552,6 +498,8 @@ class MKUtils {
                     "responseType": 'json'
                 })
                 .then(resp => {
+                    let msgData = resp.body.data;
+                    console.log(msgData[0].type, msgData[0].status)
                     resolve(resp.body.data)               
                 })
                 .catch(err => {
@@ -908,11 +856,13 @@ let startNow = setTimeout(async () => {
         /////////////////////////////////////////////////
         /////////////////////////////////////////////////        
         _mk.SubscribeToTopic(id, 'user.update', '1', { user_id: mKiwi[0].id })
+        _mk.SubscribeToTopic(id, 'stream.online', '1', { broadcaster_user_id: mKiwi[0].id })
+        _mk.SubscribeToTopic(id, 'stream.offline', '1', { broadcaster_user_id: mKiwi[0].id })
         _mk.SubscribeToTopic(id, 'channel.update', '2', { broadcaster_user_id: mKiwi[0].id })
         _mk.SubscribeToTopic(id, 'channel.follow', '2', { broadcaster_user_id: mKiwi[0].id, moderator_user_id: mKiwi[0].id })
         _mk.SubscribeToTopic(id, 'channel.raid', '1', { to_broadcaster_user_id: mKiwi[0].id })
-        _mk.SubscribeToTopic(id, 'channel.subscribe', '1', { broadcaster_user_id: mKiwi[0].id })
-        _mk.SubscribeToTopic(id, 'channel.subscription.gift', '1', { broadcaster_user_id: mKiwi[0].id })
+        // _mk.SubscribeToTopic(id, 'channel.subscribe', '1', { broadcaster_user_id: mKiwi[0].id })
+        // _mk.SubscribeToTopic(id, 'channel.subscription.gift', '1', { broadcaster_user_id: mKiwi[0].id })
         _mk.SubscribeToTopic(id, 'channel.bits.use', '1', { broadcaster_user_id: mKiwi[0].id })
         _mk.SubscribeToTopic(id, 'channel.chat.message', '1', { broadcaster_user_id: mKiwi[0].id, user_id: mKiwi[0].id })
         _mk.SubscribeToTopic(id, 'channel.chat.notification', '1', { broadcaster_user_id: mKiwi[0].id, user_id: mKiwi[0].id })
